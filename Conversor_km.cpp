4072b9887c272000047c7d06bcff4f6bcd71bd1c
Walisson86 <47633195+Walisson86@users.noreply.github.com>
2019-04-02 10:59:49 -0300

    Add files via upload

---
+++ Conversor_km.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+	float KM, m;
+	
+		cout<<"insira o valor em m/s: ";
+		cin>>m;
+		KM=m*3.5;
+		cout<<"Convertido em Km/s, o valor e igual a: "<<KM<<"\n";
+		
+		system("pause");
+		
+		return 0;	
+}
